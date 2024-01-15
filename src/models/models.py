7b9fb5e2c6aa59a8b8a0f7a928f73adf24172d6b
ool2o8 <ool2o8@github.com>
2024-01-16 01:17:38 +0900

    카카오 로그인 및 유저 생성

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import BIGINT, Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 import datetime
@@ -9,8 +9,8 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    kakao_id = Column(String, unique=True, index=True)
+    id = Column(BIGINT, primary_key=True)
+    kakao_id = Column(BIGINT, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.datetime.now())
@@ -18,9 +18,9 @@ class User(Base):
 class PriceLog(Base):
     __tablename__ = "price_logs"
     id = Column(Integer, primary_key=True)
-    price = Column(Integer)
-
-    items = relationship("Product", back_populates="price_logs")
+    price = Column(BIGINT)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    product = relationship("Product", backref="price_logs")
     created_at = Column(DateTime, default=datetime.datetime.now())
 
 
--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,23 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import RedirectResponse
+from sqlalchemy import insert, select
 
-from ..db.database import SessionLocal
+from ..db.database import SessionLocal, get_db
 from ..core.config import KAKAO_REST_API_KEY
-import status
+import status, requests
+from sqlalchemy.orm import Session
+from ..models.models import User
+
 router = APIRouter(
     prefix="/api/auth",
 )
 
 
-
+kakao_token_api = 'https://kauth.kakao.com/oauth/token'
 @router.get("/login/callback")
-def login():
-    return {"Hello": "callback"}
+def login(code:str, db: Session = Depends(get_db)):
+    
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': KAKAO_REST_API_KEY,
+        'redirection_uri': 'http://127.0.0.1',
+        'code': code
+    }
+    token_response = requests.post(kakao_token_api, data=data)
+    access_token = token_response.json().get('access_token')
+
+    response = requests.get(
+        'https://kapi.kakao.com/v2/user/me', headers={"Authorization": f'Bearer ${access_token}'})
+    if response.status_code==200:
+        user_info=response.json()
+        stmt=select(User).where(User.kakao_id == user_info["id"])
+        user=db.execute(stmt).scalar_one_or_none()
+        if not user:
+            stmt=insert(User).values(kakao_id=user_info["id"]).returning(User.id, User.kakao_id, User.created_at)
+            user=db.execute(stmt).scalar()
+            db.commit()
+    return user
 
 @router.get("/login")
 def login():
     REDIRECT_URI = 'http://127.0.0.1/api/auth/login/callback'
 
-    API_HOST = f'https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code&prompt=login'
+    API_HOST = f'https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code&prompt=select_account'
 
     return RedirectResponse(url=API_HOST, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
