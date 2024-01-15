43e25f5a5729aa70b164bec40c574eb1d42181cd
ool2o8 <ool2o8@github.com>
2024-01-15 22:19:44 +0900

    카카오 로그인

--- src/core/config.py
+++ src/core/config.py
@@ -3,3 +3,4 @@ import os
 POSTGRES_USER = os.getenv("POSTGRES_USER", None)
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", None)
 POSTGRES_DB = os.getenv("POSTGRES_DB", None)
+KAKAO_REST_API_KEY = os.getenv("KAKAO_REST_API_KEY", None)
--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,12 +1,23 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from ..db.database import SessionLocal
-
+from ..core.config import KAKAO_REST_API_KEY
+import status
 router = APIRouter(
     prefix="/api/auth",
 )
 
 
+
+@router.get("/login/callback")
+def login():
+    return {"Hello": "callback"}
+
 @router.get("/login")
 def login():
-    return {"Hello": "login"}
+    REDIRECT_URI = 'http://127.0.0.1/api/auth/login/callback'
+
+    API_HOST = f'https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code&prompt=login'
+
+    return RedirectResponse(url=API_HOST, status_code=status.HTTP_307_TEMPORARY_REDIRECT)
